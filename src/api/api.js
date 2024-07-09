ccb564afb981bce939644da2b1fce14b595aa473
Kris Wale <camoakohene84@gmail.com>
2024-07-09 22:23:54 +0000

    api

--- src/api/api.js
+++ src/api/api.js
@@ -3,10 +3,16 @@ import axios from 'axios'
 const baseURL = 'https://fm.wavebeep.com/v1/'
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL,
   timeout: 50000,
   headers: {
-    // 'Access-Control-Allow-Origin': 'http://localhost:5173',
     'Access-Control-Allow-Origin': baseURL
   }
+  // export const api = axios.create({
+  //   baseURL: '/api',
+  //   timeout: 50000,
+  //   headers: {
+  //     // 'Access-Control-Allow-Origin': 'http://localhost:5173',
+  //     'Access-Control-Allow-Origin': baseURL
+  //   }
 })
