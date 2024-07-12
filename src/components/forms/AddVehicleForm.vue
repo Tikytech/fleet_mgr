f19b44e23aceefd8029c416ccee02e01a82329ed
Kris Wale <camoakohene84@gmail.com>
2024-07-12 14:54:05 +0000

    vehicle api integrations

--- src/components/forms/AddVehicleForm.vue
+++ src/components/forms/AddVehicleForm.vue
@@ -2,7 +2,7 @@
   <div class="rounded-md max-w-[1000px] mx-auto bg-white overflow-hidden">
     <h3 class="bg-slate-100 p-4 font-bold">Add Vehicle</h3>
 
-    <form action="" class="p-4 space-y-4">
+    <form action="" class="p-4 space-y-4" @submit.prevent="submitForm">
       <div class="grid grid-cols-3 gap-5 mb-6">
         <!-- Upload Vehicle Image -->
         <div class="bg-slate-100 row-span-3 relative">
@@ -21,83 +21,84 @@
           </div>
         </div>
 
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Vehicle Model</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Vehicle Make</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Vehicle Year</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Registration Number</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Engine Number</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Chassis Number</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Permissible Gross Weight</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Net Weight</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Supplier</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Purchase Date</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Price Paid Fob</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Price Paid Freight</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
-        <!-- Vehicle Model -->
-        <div class="">
-          <label for="name" class="">Price Paid Cif</label>
-          <input required class="input mt-1" type="text" id="name" placeholder="" />
-        </div>
+        <!-- Form inputs -->
+        <template v-for="data in vehicleFormData" :key="data.v_model">
+          <div class="">
+            <label :for="data.v_model" class="capitalize">{{ data.name }}</label>
+            <input
+              required
+              class="input mt-1"
+              type="text"
+              :id="data.v_model"
+              :placeholder="data.placeholder"
+              @input="updateVehicleData($event, data.v_model)"
+            />
+          </div>
+        </template>
       </div>
       <!-- buttons -->
       <div class="flex justify-end gap-2">
         <ButtonComponent text="Close" type="border" @click="$emit('close')" typeButton="button" />
-        <ButtonComponent text="Add Vehicle" type="success" :loading="false" />
+        <ButtonComponent text="Add Vehicle" type="success" :loading="vehicleStore.loading" />
       </div>
     </form>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import ButtonComponent from '../ui/ButtonComponent.vue'
+import { useVehicleStore } from '@/stores/vehicle'
+import { vehicleFormData } from '@/data/data'
+
+const vehicleStore = useVehicleStore()
+const emit = defineEmits(['close'])
+const vehicleData = ref({
+  reg_no: '',
+  engine_no: '',
+  chassis_no: '',
+  make: '',
+  year: '',
+  model: '',
+  permissible_gross_weight: '',
+  net_weight: '',
+  supplierId: null,
+  date_purchased: '',
+  price_paid_fob: '',
+  price_paid_freight: '',
+  price_paid_cif: ''
+})
+
+function updateVehicleData(e, model) {
+  vehicleData.value[model] = e.target.value
+}
+
+async function submitForm() {
+  console.log(vehicleData.value)
+  await vehicleStore.addVehicle(vehicleData.value)
+  if (vehicleStore.isSuccessful) {
+    await vehicleStore.getAllVehicles()
+    vehicleData.value = {
+      reg_no: '',
+      engine_no: '',
+      chassis_no: '',
+      make: '',
+      year: '',
+      model: '',
+      permissible_gross_weight: '',
+      net_weight: '',
+      supplierId: null,
+      date_purchased: '',
+      price_paid_fob: '',
+      price_paid_freight: '',
+      price_paid_cif: ''
+    }
+    emit('close')
+    console.log('emmiting')
+  } else {
+    console.log('something went wrong teribli')
+  }
+}
 </script>
 
 <style scoped></style>
---
+++ src/data/data.js
@@ -0,0 +1,65 @@
+export const vehicleFormData = [
+  {
+    v_model: 'reg_no',
+    name: 'registration number',
+    input_type: 'text',
+    placeholder: 'Enter registration number'
+  },
+  {
+    v_model: 'engine_no',
+    name: 'engine number',
+    input_type: 'text',
+    placeholder: 'Enter engine number'
+  },
+  {
+    v_model: 'chassis_no',
+    name: 'chassis number',
+    input_type: 'text',
+    placeholder: 'Enter chassis number'
+  },
+  { v_model: 'make', name: 'make', input_type: 'text', placeholder: 'Enter make' },
+  { v_model: 'year', name: 'year', input_type: 'text', placeholder: 'Enter year' },
+  { v_model: 'model', name: 'model', input_type: 'text', placeholder: 'Enter model' },
+  {
+    v_model: 'permissible_gross_weight',
+    name: 'permissible gross weight',
+    input_type: 'text',
+    placeholder: 'Enter permissible gross weight'
+  },
+  {
+    v_model: 'net_weight',
+    name: 'net weight',
+    input_type: 'text',
+    placeholder: 'Enter net weight'
+  },
+  {
+    v_model: 'supplierId',
+    name: 'supplier Id',
+    input_type: 'text',
+    placeholder: 'Enter supplier Id'
+  },
+  {
+    v_model: 'date_purchased',
+    name: 'date purchased',
+    input_type: 'text',
+    placeholder: 'Enter date purchased'
+  },
+  {
+    v_model: 'price_paid_fob',
+    name: 'price paid fob',
+    input_type: 'text',
+    placeholder: 'Enter price paid FOB'
+  },
+  {
+    v_model: 'price_paid_freight',
+    name: 'price paid freight',
+    input_type: 'text',
+    placeholder: 'Enter price paid freight'
+  },
+  {
+    v_model: 'price_paid_cif',
+    name: 'price paid cif',
+    input_type: 'text',
+    placeholder: 'Enter price paid CIF'
+  }
+]
