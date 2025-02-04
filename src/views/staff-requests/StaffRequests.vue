77f357fe98c43ee96f4901505ec2072f88271263
Christian Amoakohene <camoakohene84@gmail.com>
2025-02-04 18:51:27 +0000

    requests

--- src/views/staff-requests/StaffRequests.vue
+++ src/views/staff-requests/StaffRequests.vue
@@ -13,7 +13,9 @@
 
                 <SearchAndButtonBar button-text="Request a vehicle" :filter="true" @add="showAdd = true" />
 
-                <StaffRequestTable @add="showAdd = true" />
+                <!-- <StaffRequestTable @add="showAdd = true" /> -->
+                <TableComponent :table-data="tableData" :table-head="tableHead" @add="showAdd = true"
+                    :loading="requestStore.loading" :badge="{ column: 'status' }" :get-status="getStatus" />
             </div>
         </main>
     </div>
@@ -24,10 +26,53 @@ import TopNavigation from '@/components/layout/topnav/TopNavigation.vue';
 import RequestVehicleForm from '@/components/forms/RequestVehicleForm.vue';
 import ModalComponent from '@/components/ui/ModalComponent.vue';
 import SearchAndButtonBar from '@/components/ui/SearchAndButtonBar.vue'
-import { ref } from 'vue'
-import StaffRequestTable from '@/components/tables/staff-request/StaffRequestTable.vue';
+// import StaffRequestTable from '@/components/tables/staff-request/StaffRequestTable.vue';
+import { onMounted, ref, computed } from 'vue'
+import { useRequestStore } from '@/stores/requests';
+import TableComponent from '@/components/tables/TableComponent.vue';
+import dayjs from 'dayjs';
 
 const showAdd = ref(false)
+const requestStore = useRequestStore()
+
+const tableData = computed(() => {
+    return requestStore.requests.map(item => {
+        return {
+            requestId: item?.id || "N/A",
+            college: item?.college || "N/A",
+            tripDate: dayjs(item?.trip_date).format("MMM DD, YYYY") || "N/A",
+            return_date: dayjs(item?.return_date).format("MMM DD, YYYY") || "N/A",
+            passengers: item?.no_of_passengers || "N/A",
+            status: item?.status || "N/A",
+        }
+    })
+})
+
+const tableHead = [
+    { title: 'Request ID' },
+    { title: 'College' },
+    { title: 'Trip Date' },
+    { title: 'Return Date' },
+    { title: 'Passengers' },
+    { title: 'Status' },
+]
+
+function getStatus(status) {
+    switch (status.toLowerCase()) {
+        case 'approved':
+            return 'success';
+        case 'pending':
+            return 'warning';
+        case 'rejected':
+            return 'danger';
+        default:
+            return '';
+    }
+}
+
+onMounted(async () => {
+    await requestStore.getAllRequests()
+})
 
 </script>
 
